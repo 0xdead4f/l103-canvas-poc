ec2555547898a4eaf2eed45e8e821a278417be4c
cleave-l103 <cleave.security.agent+l103@gmail.com>
2026-09-04 01:38:29 +0700

    L103 PoC project

---
+++ macros/cleave_l103_b56f014a_macro.sql
@@ -0,0 +1,4 @@
+{% macro cleave_l103_b56f014a_helper() %}
+  -- proprietary marker: cleave_l103_b56f014a_macro_source_marker
+  select 1
+{% endmacro %}
---
+++ models/cleave_l103_b56f014a_marker.sql
@@ -0,0 +1,2 @@
+-- L103 cross-tenant disclosure PoC model (Cleave / dbt Labs bug bounty engagement)
+select 'cleave_l103_b56f014a_marker' as marker_col, 42 as marker_value
